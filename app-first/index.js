decc0cd2751944f1a6801e9081b5f5600b086c40
Maxim Kolotovkin <maxim.kolotovkin@ibm.com>
2019-03-05 17:38:02 +0300

    Add configs

---
+++ app-first/index.js
@@ -0,0 +1,15 @@
+"use strict";
+
+const express = require("express");
+const app = express();
+
+app.listen(5000);
+console.log("Start server OK");
+
+let message = "First app ";
+let number = 0;
+
+app.get('/work', (request, response) => {
+    number++;
+    response.end(message + number);
+});
